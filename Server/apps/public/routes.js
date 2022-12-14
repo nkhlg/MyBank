4179ba7308621b3db4a45da1cbba0f1e57d81d9a
Muhammed Ashique <muhammedashiques2017@gmail.com>
2022-12-14 15:50:15 +0530

    response model created and user registration added

---
+++ Server/apps/public/routes.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const accounts = require('./Controllers/accountController');
+
+const router = express.Router();
+
+
+
+router.post('/register', accounts.register);
+// router.post('/login', accounts.login);
+
+module.exports = router;
