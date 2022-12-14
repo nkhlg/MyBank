b3f3b21bcc39d0b4562fb1f492ae5ee20972bc7a
Hrithik Das M <hrithik.rmcaa2023@saintgits.org>
2022-12-14 15:35:20 +0530

    api for loan approval and user approval

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const publicRoutes = require('./apps/public/routes');
-const userRoutes = require('./apps/user/routes');
+// const publicRoutes = require('./apps/public/routes');
+// const userRoutes = require('./apps/user/routes');
+const adminRoutes = require('./apps/admin/routes');
 const dotEnv = require('dotenv');
-const authMiddleware = require('./middlewares/authMiddleware');
+// const authMiddleware = require('./middlewares/authMiddleware');
 const cors = require('cors');
 
 dotEnv.config();
 
 const app = express();
 
-app.use(cors({origin: '*'}));
-app.use(authMiddleware);
-app.use(bodyParser.json({inflate: true}));
+// app.use(cors({origin: '*'}));
+// app.use(authMiddleware);
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/api/v1', publicRoutes);
-app.use('/api/v1/user', userRoutes);
+// app.use('/api/v1', publicRoutes);
+// app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/admin',adminRoutes);
 
 app.listen(80);
