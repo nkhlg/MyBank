6b2411e764f9501381a4fffc83bd00c8252e981d
Raj Kishore yadav <raj.yadav@experionglobal.com>
2022-12-15 18:10:49 +0530

    Completed
    I done 2  API for  get all user and loan details for admin. please review

--- Server/apps/admin/routes.js
+++ Server/apps/admin/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const admin = require('./Controllers/adminController');
+const UserDetail = require('./Controllers/AllUserDetailController');  
+const LoanDetail = require('./Controllers/AllLoanDetailController');  
 
 const router = express.Router();
+
 router.post('/approvalStatus/:id',admin.userApproval);
 router.post('/loanApprovalStatus/:userId',admin.loanApproval);
+
+router.get('/userdetail',UserDetail.GetAll);   //Get all user details route
+router.get('/loandetail',LoanDetail.LoanDetail);   //Get all loan details route
+
+
 
 module.exports = router;
